5fc79ecdc5027e5fe2de404036b11d383c1998de
narayanbanke <60840004+narayanbanke@users.noreply.github.com>
2021-01-16 13:37:37 +0530

    19

---
+++ CRUDCore/Models/CRUDContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CRUDCore.Models;
+namespace CRUDCore.Models
+{
+    public class CRUDContext : DbContext
+    {
+        public CRUDContext(DbContextOptions<CRUDContext> options) : base(options)
+        {
+        }
+        public DbSet<CRUDCore.Models.Customer> Customer { get; set; }
+    }
+}
